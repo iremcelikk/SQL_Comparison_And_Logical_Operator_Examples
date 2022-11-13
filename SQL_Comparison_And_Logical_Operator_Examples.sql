df468fb0fd9eb7b61a9538c6ace6e5f1a91f68f2
iremcelikk <102186287+iremcelikk@users.noreply.github.com>
2022-11-13 13:40:58 +0300

    Add files via upload

---
+++ SQL_Comparison_And_Logical_Operator_Examples.sql
@@ -0,0 +1,5 @@
+-- 1-Select the data in the film table with title and description columns
+--select title, description from film;
+
+-- 2-Select data in all columns in the film table with the movie length greater than 60 AND less than 75.
+--select * from film where length>60 and length<75;
